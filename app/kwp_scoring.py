5bfd4dca7111d4e222809bd563a6f997277586c9
Pierce Saly <psaly6@gmail.com>
2023-07-20 11:26:02 -0400

    Scottish

--- app/kwp_scoring.py
+++ app/kwp_scoring.py
@@ -12,14 +12,14 @@ COUNTING_SCORES = 4
 KWP_BONUSES = [10, 5, 3, 2, 1]
 
 kwp_teams = {
-    'Saly': ['Denny McCarthy', 'Stephan Jaeger', 'Alex Smalley', 'Nick Taylor', 'Patrick Rodgers', 'K.H. Lee', 'Emiliano Grillo'],
-    'Harv': ['Nick Hardy', 'Adam Schenk', 'Séamus Power', 'Taylor Montgomery', 'Joseph Bramlett', 'Cameron Champ', 'Zach Johnson'],
-    "O'Leary": ['Chris Kirk', 'Adam Svensson', 'Taylor Moore', 'Beau Hossler', 'Adam Long', 'Keith Mitchell', 'Joel Dahmen'],
-    'Corby': ['Cameron Young', 'Sepp Straka', 'Adam Hadwin', 'J.T. Poston', 'Matt Kuchar', 'Russell Henley', 'Lucas Glover']
+    'Saly': ['Rory McIlroy', 'Viktor Hovland', 'Wyndham Clark', 'Max Homa', 'Tom Kim', 'Gary Woodland', 'Sahith Theegala'],
+    'Harv': ['Scottie Scheffler', 'Xander Schauffele', 'Justin Rose', 'Tyrrell Hatton', 'Jordan Spieth', 'Sungjae Im', 'Alex Noren'],
+    "O'Leary": ['Justin Thomas', 'Sam Burns', 'Rickie Fowler', 'Adam Scott', 'Kurt Kitayama', 'Austin Eckroat', 'Thomas Detry'],
+    'Corby': ['Patrick Cantlay', 'Aaron Rai', 'Matt Fitzpatrick', 'Tommy Fleetwood', 'Shane Lowry', 'Corey Conners', 'Cameron Davis']
 }
 
-ESPN_URL = 'https://www.espn.com/golf/leaderboard'
-# ESPN_URL = 'https://www.espn.com/golf/leaderboard/_/tournamentId/401465535'
+# ESPN_URL = 'https://www.espn.com/golf/leaderboard'
+ESPN_URL = 'https://www.espn.com/golf/leaderboard/_/tournamentId/401465537'
 
 
 def main():
